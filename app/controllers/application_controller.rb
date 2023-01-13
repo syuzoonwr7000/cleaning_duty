59fe2255554c99e2433fa8adc823e172141a7b22
syuzoonwr7000 <syuzoonwr7000@gmail.com>
2023-01-13 17:32:46 +0900

    changed_to_posgresql

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  @@start = Date.new(2022,11,19)
+
+  def self.count_days
+    today = Date.today
+    days = (@@start..today).select{|d|d.workday?}
+    days.count
+  end
+
 end
--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -1,56 +1,49 @@
 class TodayController < ApplicationController
     @@start = Date.new(2022,11,19)
+    @@days = count_days 
 
   def index
     member_array
-    count_days
-    @student.rotate!(@days)
+    @student.rotate!(@@days)
     
-    if @days%3 == 0
+    if @@days%3 == 0
       @student.each_index do |i|
-        @student[i].rotate!(@days/3)
+        @student[i].rotate!(@@days/3)
       end
     else
       @student.each_index do |i|
-        @student[i].rotate!(@days/3)
+        @student[i].rotate!(@@days/3)
       end
     end 
   end
 
-  def show
+  def days_back
+    @@days - 1 
   end
 
-
-  def edit
+  def self.count_days
+    today = Date.today
+    days = (@@start..today).select{|d|d.workday?}
+    days.count
   end
 
-  def new
-    index
-    Member.create(member: @student[0][0..5].join(','),jihanki: @student[0][6],daikyousitu:@student[0][7],date:Date.today)
-    @member = Member.select("member") 
-  end
 
   private
 
   def member_array
-    numbers = Student.select("number")
+    numbers = (1..24).to_a
     group1 = []
     group2 = []
     group3 = []
     @student = []
 
     8.times do |i|
-       group1.push(numbers[i].number)
-       group2.push(numbers[i+8].number)
-       group3.push(numbers[i+16].number)
+       group1.push(numbers[i])
+       group2.push(numbers[i+8])
+       group3.push(numbers[i+16])
     end
     @student.append(group1,group2,group3)
     
   end
   
-  def count_days
-    today = Date.today
-    days = (@@start..today).select{|d|d.workday?}
-    @days = days.count
-  end
 end
